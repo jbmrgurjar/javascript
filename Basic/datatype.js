f8ab81346474adf4b37370ff5e196ba023aabf78
Vishnugurjar <145193338+jbmrgurjar@users.noreply.github.com>
2024-07-31 08:40:10 +0000

    variables and datatype

---
+++ Basic/datatype.js
@@ -0,0 +1,13 @@
+// Number =>2 to power 53
+// BigInt=>
+// String =>"jbmr"
+// Boolean=>true/false
+// null 
+// undefined
+// Symbol=>unquie
+
+let age=25;
+let name="jbmr";
+let loggedin=true;
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ Basic/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12354;
+let accountgmail = "gmail.com";
+var passoword = "jbmr";
+city="Gwalior";
+let v;
+
+
+
+//   accountId=3434 //not allowed
+//   console.log(accountId);
+ accountgmail = "gmail.com1"
+passoword="vishnu";
+city="morena"
+
+console.table([accountId,accountgmail,passoword,city,v]);
