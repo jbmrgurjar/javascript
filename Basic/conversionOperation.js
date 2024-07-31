d16c31d43051540e00443febedf3e3fd748872e3
Vishnugurjar <145193338+jbmrgurjar@users.noreply.github.com>
2024-07-31 12:23:46 +0000

    operation

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,37 @@
+// let score="45"
+// let score="45a"
+let score=true
+// console.log(typeof score);
+// let value=Number(score);
+
+// console.log(typeof value);
+// console.log(value);
+let a=0;
+// let b=Boolean(typeof a);
+// console.log(b);
+// 1=> true; 0=>false
+// ""=>false; "jbmr"=>true
+let Number=55;
+let string=String(Number);
+console.log(string);
+console.log(typeof string);
+
+
+
+// **************************************Operations************************************************
+// let value=3;
+// let negvalue=-value
+// console.log(negvalue);
+// console.log(3**2); power
+// console.log("1"+"3");
+// console.log(1+"3");
+// console.log("1"+3+4);
+// console.log(1+3+"4");
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+// let count=2;
+// count--;
+// console.log(count);
+// count++;
+// console.log(count);
