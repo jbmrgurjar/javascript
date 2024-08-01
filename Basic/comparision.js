67ff94e19db8548899742f92667ecb7eee807039
Vishnugurjar <145193338+jbmrgurjar@users.noreply.github.com>
2024-08-01 05:51:07 +0000

    datatype

---
+++ Basic/comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+
+//***************** *== operater*****************
+//***************** *=== operater*****************
+
+console.log("2"==2);// not data type check
+console.log("2"===2); //data type check
---
+++ Basic/datatype_1.js
@@ -0,0 +1,26 @@
+// 1  primitive 
+// 7 type: String ,Number, Boolean ,null, undefind,Symbol,BigInt
+
+const  score=100;
+const scorevalue=100.3;
+const isloggedin=false;
+const outsidetemp=null;
+let a;
+// console.log(a);
+// Reference (non-primitive)
+
+// Array,Object,Function
+const name=["vishnu","jbmr","gurjar"]
+
+let obj={
+    name:"jbmr",
+    id:1,
+
+
+}
+// console.log(typeof obj);
+let myfunction=function(){
+    console.log("hello");
+}
+
+console.log(myfunction);//body
